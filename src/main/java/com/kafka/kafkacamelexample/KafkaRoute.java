2debf92f340ba5314becf44d37b43728ba12efb2
Abhishek Vishnoi <abvishno@redhat.com>
2024-02-09 16:01:51 +0530

    first commit

--- src/main/java/com/kafka/kafkacamelexample/KafkaRoute.java
+++ src/main/java/com/kafka/kafkacamelexample/KafkaRoute.java
@@ -29,13 +29,13 @@ public class KafkaRoute  extends RouteBuilder {
                         "&sslKeystorePassword=password" +
                         "&sslTruststoreLocation=/home/jboss/trust.jks" +
                         "&sslTruststorePassword=password" +
-                        //"&sslKeyPassword=password" +
+                        "&sslKeyPassword=N1UG7UMpVi4drEXjLl3C3dlDjmGrjKGM" +
                         "&securityProtocol=SSL");
 
         // Kafka Consumer
         from("kafka:{{topic}}?brokers={{broker}}&sslKeystoreLocation=/home/jboss/kafka.jks" +
                 "&sslKeystorePassword=password" +
-               // "&sslKeyPassword=password" +
+                 "&sslKeyPassword=N1UG7UMpVi4drEXjLl3C3dlDjmGrjKGM" +
                 "&sslTruststoreLocation=/home/jboss/trust.jks" +
                 "&sslTruststorePassword=password" +
                 "&securityProtocol=SSL")
